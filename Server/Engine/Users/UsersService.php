6145fb227676c9e4c4848250b0348b2f4073f06e
szymongor <gornioczek.szymon@gmail.com>
2017-05-28 15:47:24 +0200

    UserService - Registration

--- Server/Engine/Users/UsersService.php
+++ Server/Engine/Users/UsersService.php
@@ -18,7 +18,7 @@
           $response = array('status' =>'Ok' ,'login' => $login, 'id'=> $user['id']);
         }
         else{
-          $response = array('status' =>'Error' , "Message" =>"Wrong password:");
+          $response = array('status' =>'Error' , "Message" =>"Wrong password");
         }
       }
       else{
@@ -29,8 +29,32 @@
     }
 
     public function registerUser($login, $password, $email){
+
+      if((strlen($login) < 3) || (strlen($login) >20)){
+  			$response = array('status' => 'Error', 'Message' => "Nick should have from 3 to 20 characters!");
+        return $response;
+  		}
+      if(ctype_alnum($login)==false){
+        $response = array('status' => 'Error', 'Message' => "Nick should contain only letters and numbers!");
+        return $response;
+  		}
+      $emailB = filter_var($email,FILTER_SANITIZE_EMAIL);
+  		if((filter_var($emailB,FILTER_VALIDATE_EMAIL)==false) || ($emailB != $email))
+  		{
+        $response = array('status' => 'Error', 'Message' => "Please enter a valid email");
+        return $response;
+  		}
+      $user = $this->userDao->getUserByLogin($login);
+      if($user != null){
+        $response = array('status' => 'Error', 'Message' => 'Nickname already exists');
+        return $response;
+      }
+
       $pass_hash = password_hash($password, PASSWORD_DEFAULT);
-      $response = array('login' => $login, '$pass' => $pass_hash, '$email' => $email);
+
+
+      $this->userDao->addUser($login, $pass_hash, $email);
+      $response = array('status' => 'Ok', 'Message' => 'Succes. A new account has been created.');
       return $response;
 
     }
