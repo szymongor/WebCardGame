3fda7f4bdb849ba019d65d53bfda1c7c0a690f50
szymongor <gornioczek.szymon@gmail.com>
2017-06-01 01:05:49 +0200

    GamesDao - manage games info and files

--- Server/Engine/Games/GamesDao.php
+++ Server/Engine/Games/GamesDao.php
@@ -4,27 +4,47 @@
   class GamesDao{
 
     private $gamesFolder = "../../CurrentGamesFiles/";
+    private $db_connect;
 
     public function __construct(){}
 
-    private function generateGameId(){
-      $randId = substr(md5(microtime()),0,15);
-      $game = $this->getGameById($randId);
-      while($game != null){
-        $randId = substr(md5(microtime()),0,15);
-        $game = $this->getGameById($randId);
+    private function startConnection(){
+      global $host, $db_user, $db_password, $db_name;
+      $this->db_connect = @new mysqli($host, $db_user, $db_password, $db_name);
+      if ($this->db_connect->connect_error) {
+        return false;
+        //die("Connection failed: " . $this->db_connect->connect_error);
       }
-      return $randId;
+      else{
+        return true;
+      }
     }
 
     public function getAllGames(){
-      $gameFilePath = $this->gamesFolder."/Game1.json";
-      $gamesFile = fopen($gameFilePath, "r");
-      $games = json_decode(fread($gamesFile,filesize($gameFilePath)),true);
+      $this->startConnection();
+      $queryStr = sprintf("SELECT * FROM `games`");
+    	$result = @$this->db_connect->query($queryStr);
+      if (!$result) {
+          throw new Exception("Database Error [{$this->db_connect->errno}] {$this->db_connect->error}");
+      }
+    	$games = $result->fetch_assoc();
+      mysqli_close($this->db_connect);
       return $games;
     }
 
-    public function getGameById($gameId){
+    public function getGameInfoById($gameId){
+      $this->startConnection();
+      $queryStr = sprintf("SELECT * FROM `games` WHERE id = \"%s\"",$gameId);
+    	$result = @$this->db_connect->query($queryStr);
+      if (!$result) {
+          throw new Exception("Database Error [{$this->db_connect->errno}] {$this->db_connect->error}");
+      }
+    	$row = $result->fetch_assoc();
+      mysqli_close($this->db_connect);
+      return $row;
+    }
+
+    public function getGameFileById($gameId){
       $gameFilePath = $this->gamesFolder.$gameId.".json";
       try{
         $gamesFile = @fopen($gameFilePath, "r");
@@ -35,28 +55,100 @@
       } catch(Exception $e){
         $game= null;
       }
-
       return $game;
     }
 
-    public function createGame(){
-      $newGameId = $this->generateGameId();
-      $gameFilePath = $this->gamesFolder."/".$newGameId.".json";
-      $gamesFile = fopen($gameFilePath, "w");
-      $game = array('Message' => "Yo2");
-      fwrite($gamesFile,json_encode($game));
-      fclose($gamesFile);
-      //return $game;
+    public function createNewGame($gameID, $playerId){
+      $gameInfo = $this->getGameInfoById($gameID);
+      if($gameInfo == null){
+        $this->addGameToDB($gameID);
+        $this->setPlayerPlayingGame($playerId,$gameID);
+        $gameFilePath = $this->gamesFolder."/".$gameID.".json";
+        $gamesFile = fopen($gameFilePath, "w");
+        $game = array('Message' => "Yo2");
+        fwrite($gamesFile,json_encode($game));
+        fclose($gamesFile);
+      }
+      else{
+        throw new Exception("Game already exist");
+      }
     }
 
+    public function getPlayerGame($playerId){
+
+    }
+
+    private function addGameToDB($gameId){
+      $this->startConnection();
+    	$queryStr = sprintf("INSERT INTO `games`(`id`, `generationDate`) VALUES (\"%s\",%s)",$gameId,time());
+    	$result = @$this->db_connect->query($queryStr);
+      if (!$result) {
+          throw new Exception("Database Error [{$this->db_connect->errno}] {$this->db_connect->error}");
+      }
+      mysqli_close($this->db_connect);
+    }
+
+    public function setPlayerPlayingGame($playerId, $gameId){
+      $this->startConnection();
+    	$queryStr = sprintf("INSERT INTO `plays`(`user`, `game`) VALUES (%s,\"%s\")",$playerId,$gameId);
+    	$result = @$this->db_connect->query($queryStr);
+      if (!$result) {
+          throw new Exception("Database Error [{$this->db_connect->errno}] {$this->db_connect->error}");
+      }
+      mysqli_close($this->db_connect);
+    }
+
+    public function unsetPlayerPlayingGame($playerId){
+      $this->startConnection();
+    	$queryStr = sprintf("DELETE FROM `plays` WHERE user = %s",$playerId);
+    	$result = @$this->db_connect->query($queryStr);
+      if (!$result) {
+          throw new Exception("Database Error [{$this->db_connect->errno}] {$this->db_connect->error}");
+      }
+      mysqli_close($this->db_connect);
+    }
+
+    public function deleteGame($gameId){
+      $this->unsetPlayingGame($gameId);
+      $this->deleteGameInfo($gameId);
+      $this->deleteGameFile($gameId);
+    }
+
+    public function unsetPlayingGame($gameId){
+      $this->startConnection();
+    	$queryStr = sprintf("DELETE FROM `plays` WHERE game = \"%s\"",$gameId);
+    	$result = @$this->db_connect->query($queryStr);
+      if (!$result) {
+          throw new Exception("Database Error [{$this->db_connect->errno}] {$this->db_connect->error}");
+      }
+      mysqli_close($this->db_connect);
+    }
+
+    private function deleteGameFile($gameId){
+      $gameFilePath = $this->gamesFolder."/".$gameId.".json";
+      if (file_exists($gameFilePath)) {
+        unlink($gameFilePath);
+      }
+    }
+
+    private function deleteGameInfo($gameId){
+      $this->startConnection();
+    	$queryStr = sprintf("DELETE FROM `games` WHERE id = \"%s\"",$gameId);
+    	$result = @$this->db_connect->query($queryStr);
+      if (!$result) {
+          throw new Exception("Database Error [{$this->db_connect->errno}] {$this->db_connect->error}");
+      }
+      mysqli_close($this->db_connect);
+    }
 
 
   }
 
   $gamesDao = new GamesDao();
-  $response = $gamesDao->getGameById("8743eqc4rq3");
-  $gamesDao->createGame();
-  echo json_encode($response);
-
+  //$gamesDao->unsetPlayerPlayingGame(1);
+  //$gamesDao->createNewGame("myNewGame",1);
+  $gamesDao->deleteGame("myNewGame");
+  //$response = $gamesDao->getAllGames();
+  //echo json_encode($response);
 
 ?>
