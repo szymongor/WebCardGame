61f23c295016eb9425156ce542e4b2d38ed0eb8c
szymongor <gornioczek.szymon@gmail.com>
2017-06-14 14:27:54 +0200

    CardsDao

--- Server/Engine/Card/Card.php
+++ Server/Engine/Card/Card.php
@@ -3,7 +3,7 @@
 
   class Card{
 
-    private $name;
+    private $title;
     private $type;
     private $image;
     private $cost;
@@ -12,12 +12,12 @@
 
     public function __construct($cardJSON){
       $cardObj = json_decode($cardJSON,true);
-      $this->name = $cardObj['Name'];
+      $this->title = $cardObj['Title'];
       $this->type = $cardObj['Type'];
       $this->image = $cardObj['Image'];
       $this->cost = $cardObj['Cost'];
       $this->action = $cardObj['Action'];
-      $this->effects = $cardObj['Effects'];
+      //$this->effects = $cardObj['Effects'];
     }
 
 
---
+++ Server/Engine/Card/CardsDao.php
@@ -0,0 +1,36 @@
+<?php
+
+
+  class CardsDao{
+
+    private $cardsPath;
+
+
+    public function __construct(){
+      $this->cardsPath = $_SERVER['DOCUMENT_ROOT']."/CardGame/Server/Engine/Card/Cards/Cards.json";
+    }
+
+    public function getCardsFile(){
+      try{
+        $cardsFile = @fopen($this->cardsPath, "r");
+        if (!$cardsFile) {
+          throw new Exception('No such file');
+        }
+        $cards = fread($cardsFile,filesize($this->cardsPath));
+        //$game = $gameState->toArray();
+      } catch(Exception $e){
+        $cards = null;
+      }
+      return json_decode($cards,true);
+
+    }
+
+  }
+
+  $cardsDao = new CardsDao();
+  $response = $cardsDao->getCardsFile()[50];
+  echo(json_encode($response));
+
+
+
+?>
